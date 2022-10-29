51d7082a8986ab82b60b163ae34c7cb7081385d5
kjuulh <contact@kjuulh.io>
2022-10-29 18:03:36 +0200

    updateloadtemplate

--- uploadtemplate.sh
+++ uploadtemplate.sh
@@ -1,1 +1,4 @@
-drone template add --namespace kjuulh --name dagger_go_template.yaml --data @dagger_go_template.yaml
+#!/bin/bash
+
+drone template add --namespace kjuulh --name dagger_go_template.yaml --data @dagger_go_template.yaml || true
+drone template update --namespace kjuulh --name dagger_go_template.yaml --data @dagger_go_template.yaml
