5d9a6296f118886f50cedd84509c9ca74357e084
kjuulh <contact@kjuulh.io>
2022-10-30 18:46:00 +0100

    with custom build cli

---
+++ pkg/cli/custom.go
@@ -0,0 +1,19 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCustomGoBuild(command string, runf func(ctx context.Context) error) error {
+	cmd := &cobra.Command{
+		Use: fmt.Sprintf("dagger-go build %s", command),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runf(cmd.Context())
+		},
+	}
+
+	return cmd.Execute()
+}
